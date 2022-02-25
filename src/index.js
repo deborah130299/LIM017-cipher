0aaa9ce1fda40bcbefd4a7b33f5be2eaad86ef11
deborah130299 <debeframart@gmail.com>
2022-02-24 19:21:11 -0500

    :c

--- src/index.js
+++ src/index.js
@@ -1,28 +1,26 @@
-
-clearbutton.addEventListener('click', () => {
-
-    document.getElementById("input").value = '';
-    document.getElementById("offsetCipher").value = '';
-    document.getElementById("result").value = '';
-    document.getElementById("input").focus();
-}
+import cipher from './cipher.js';
 
 const cifrad = document.getElementById('Cifrado');
 const Descifrad = document.getElementById('Descifrado');
 
-
-Cifrad.addEventListener('click', () => {  //Codificar
-    const string = document.getElementById('text').value;
-    const offset = document.getElementById('offset').value;
+cifrad.addEventListener('click', () => {  //Codificar
+    const string = document.getElementById("input").value;
+    const offset = document.getElementById('offsetCipher').value;
     const textCod = cipher.encode(offset, string);
-    document.getElementById('input').innerText = textCod;
-    document.getElementById('result').innerText = 'Texto Cifrado';
-});
-Descifrad.addEventListener('click', () => { //Decodificar
-    const string = document.getElementById('text').value;
-    const offset = document.getElementById('offset').value;
-    const textDeCod = cipher.decode(offset, string);
-    document.getElementById('input').innerText = textDeCod;
-    document.getElementById('result').innerText = 'Texto Descifrado';
+    document.getElementById('result').innerText = textCod;
+  
 });
 
+Descifrad.addEventListener('click', () => { //Decodificar
+    const string = document.getElementById('input').value; 
+    const offset = document.getElementById('offsetCipher').value;
+    const textDeCod = cipher.decode(offset, string);
+    document.getElementById('result').innerText = textDeCod;
+   
+});
+const clearbutton = document.getElementById('clearbutton');
+clearbutton.addEventListener('click', () => { 
+    document.getElementById('result').innerText = '';
+    document.getElementById('offsetCipher').value = '';
+    document.getElementById('input').value = '';
+});
